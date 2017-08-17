d4626e61ce0afa3eafa4e15ffdc0560493df737d
Al Jachimiak <aljachimiak@gmail.com>
2017-08-17 10:38:28 -0400

    play 1000 hands

--- war-cards.js
+++ war-cards.js
@@ -245,66 +245,70 @@ class Game {
 }
 
 function playGame() {
-const game = new Game();
+	const game = new Game();
 
-while(!game.gameComplete) {
-	game.playHand(false);
+	while(!game.gameComplete) {
+		game.playHand(false);
+	}
+
+	const totalTimeSec = game.gameEnd - game.gameStart;
+	const winningPlayer = game.players[game.gameWinner];
+	const loser = game.gameWinner === 1 ? 0 : 1;
+	const losingPlayer = game.players[loser];
+
+	const winnerHandValue = winningPlayer.beginningStats.handValue;
+	const loserHandValue = losingPlayer.beginningStats.handValue;
+
+	const winnerHighValue = winningPlayer.beginningStats.highValues;
+	const loserHighValue = losingPlayer.beginningStats.highValues;
+	const code = document.createElement("code");
+
+	let report = "============ GAME OVER =============\n";
+	report += `Game Stats:\n`;
+	report += `Total Hands: ${game.numHands}\n`;
+	report += `Elapsed Time: ${totalTimeSec} ms\n`;
+	report += `Hands per ms: ${game.numHands / totalTimeSec}\n`;
+	report += `Number of Ties: ${game.numTies}\n`;
+	report += '-------------------------------------\n';
+	report += `Winner: ${winningPlayer.name}\n`;
+	report += `Number of Shuffles: ${winningPlayer.shuffles.length}\n`;
+	report += `Shuffles: ${winningPlayer.shuffles.join(', ')}\n`;
+	Object.keys(winningPlayer.beginningStats).forEach(stat => {
+		report += `${stat}: ${winningPlayer.beginningStats[stat]}\n`;
+	})
+	report += `Hand Value: winner ${winnerHandValue} : ${loserHandValue}\n`;
+	report += `High Hand Value: winner ${winnerHighValue} : ${loserHighValue}\n`;
+
+	report += '=====================================\n';
+
+	return report;
 }
 
-const totalTimeSec = game.gameEnd - game.gameStart;
-const winningPlayer = game.players[game.gameWinner];
-const loser = game.gameWinner === 1 ? 0 : 1;
-const losingPlayer = game.players[loser];
+let games = [];
 
-const winnerHandValue = winningPlayer.beginningStats.handValue;
-const loserHandValue = losingPlayer.beginningStats.handValue;
-
-const winnerHighValue = winningPlayer.beginningStats.highValues;
-const loserHighValue = losingPlayer.beginningStats.highValues;
-const code = document.createElement("code");
-
-let report = "============ GAME OVER =============\n";
-report += `Game Stats:\n`;
-report += `Total Hands: ${game.numHands}\n`;
-report += `Elapsed Time: ${totalTimeSec} ms\n`;
-report += `Hands per ms: ${game.numHands / totalTimeSec}\n`;
-report += `Number of Ties: ${game.numTies}\n`;
-report += '-------------------------------------\n';
-report += `Winner: ${winningPlayer.name}\n`;
-report += `Number of Shuffles: ${winningPlayer.shuffles.length}\n`;
-report += `Shuffles: ${winningPlayer.shuffles.join(', ')}\n`;
-Object.keys(winningPlayer.beginningStats).forEach(stat => {
-	report += `${stat}: ${winningPlayer.beginningStats[stat]}\n`;
-})
-report += `Hand Value: winner ${winnerHandValue} : ${loserHandValue}\n`;
-report += `High Hand Value: winner ${winnerHighValue} : ${loserHighValue}\n`;
-
-report += '=====================================\n';
-
-return report;
+function promiseGame() {
+	return Promise.resolve()
+	.then(() => {
+		return playGame();
+	});
 }
 
-Promise.resolve()
+for (i = 0; i < 1000; i++) {
+	games.push(promiseGame());
+}
+
+Promise.resolve(null)
 .then(() => {
-	return playGame();
+	return Promise.all(games);
 })
-.then(report => {
-	const code = document.createElement("code");
-	const info = document.getElementById("info");
-	code.innerHTML = report;
-	info.appendChild(code);
-	return null;
-})
-.then(() => {
-	return playGame();
-})
-.then(report => {
-	const code = document.createElement("code");
-	const info = document.getElementById("info");
-	code.innerHTML = report;
-	info.appendChild(code);
-	return null;
-})
+.then(reports => {
+	reports.forEach(report => {
+		const code = document.createElement("code");
+		const info = document.getElementById("info");
+		code.innerHTML = report;
+		info.appendChild(code);
+	});
+});
 
 
 
